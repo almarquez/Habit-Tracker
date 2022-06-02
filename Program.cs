98b0b84c015020e69904f4640af12f0650c3c023
Alejandro Marquez <marqueza1025@gmail.com>
2022-06-01 20:11:00 -0400

    setup database

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SQLite;
+
+string cs = @"Data Source=habit-tracker.db";
+
+CreateDatabase();
+
+void CreateDatabase()
+{
+    using (var connection = new SQLiteConnection(cs))
+    {
+        using (var tableCmd = connection.CreateCommand())
+        {
+            connection.Open();
+
+            tableCmd.CommandText = 
+                @"CREATE TABLE IF NOT EXISTS habits (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Quantity INTEGER
+                    )";
+
+            tableCmd.ExecuteNonQuery();
+        }
+    }
+}
