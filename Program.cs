06011a71409ecea719f57c02841fb1ec6adac185
Alejandro Marquez <marqueza1025@gmail.com>
2022-07-13 09:58:54 -0400

    Get user input.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 string cs = @"Data Source=habit-tracker.db";
 
 CreateDatabase();
+GetUserInput();
 
 void CreateDatabase()
 {
@@ -20,6 +21,117 @@ void CreateDatabase()
                     )";
 
             tableCmd.ExecuteNonQuery();
+        }
+    }
+}
+
+void GetUserInput()
+{
+    Console.Clear();
+    bool closeApp = false;
+    while(!closeApp)
+    {
+        Console.WriteLine("\n\nMain Menu");
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("0 = Close Application");
+        Console.WriteLine("1 = Get all habits");
+        Console.WriteLine("2 = Insert habit");
+        Console.WriteLine("3 = Delete habit");
+        Console.WriteLine("4 = Update habit");
+        Console.WriteLine("--------------------------\n");
+
+        string command = Console.ReadLine();
+
+        switch (command)
+        {
+            case "0": closeApp = true; break;
+            case "1": GetAllHabits(); break;
+            case "2": Insert(); break;
+            case "3": Delete(); break;
+            case "4": Update(); break;
+            default: Console.WriteLine("Invalid command. Please type a number between 0-4."); break;
+        }
+
+        void GetAllHabits()
+        {
+            using var connection = new SQLiteConnection(cs);
+            connection.Open();
+
+            string stm = "SELECT * FROM habits";
+            using var cmd = new SQLiteCommand(stm, connection);
+            using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine($"ID: {rdr.GetInt32(0)} Date: {rdr.GetString(1)} Quantity: {rdr.GetInt32(2)}");
+            }
+        }
+
+        void Insert()
+        {
+            Console.WriteLine("Please enter the date mm-dd-yyyy");
+            string date = Console.ReadLine();
+
+            Console.WriteLine("Please enter quantity: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            using (var con = new SQLiteConnection(cs))
+            {
+                using (var cmd = con.CreateCommand())
+                {
+                    con.Open();
+
+                    cmd.CommandText = 
+                        $"INSERT INTO habits(Date, Quantity) VALUES('{date}', '{quantity}')";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        void Update()
+        {
+            Console.WriteLine("\nPlease type in ID you wish to update.");
+            int habitId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type in new date. Format mm-dd-yyyy");
+            string newDate = Console.ReadLine();
+
+            Console.WriteLine("Type in new quantity.");
+            int newQuantity = Convert.ToInt32(Console.ReadLine());
+
+            using (var con = new SQLiteConnection(cs))
+            {
+                using (var cmd = con.CreateCommand())
+                {
+                    con.Open();
+
+                    cmd.CommandText = 
+                        $"UPDATE habits SET Date = {newDate}, Quantity = {newQuantity} WHERE Id = {habitId}";
+                    cmd.ExecuteNonQuery();
+                }
+
+                Console.WriteLine("*****Habit has been updated.*****");
+            }
+        }
+
+        void Delete()
+        {
+            Console.WriteLine("\nPlease type in the ID you wish to delete.\n");
+            int habitId = Convert.ToInt32(Console.ReadLine());
+
+            using (var con = new SQLiteConnection(cs))
+            {
+                using (var cmd = con.CreateCommand())
+                {
+                    con.Open();
+
+                    cmd.CommandText = 
+                        $"DELETE FROM habits WHERE Id = {habitId}";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("*****Your habit was deleted.*****");
         }
     }
 }
